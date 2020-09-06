a685ee3652c55541bf6c31935b3fbdb86f630511
ozair <truemail785@gmail.com>
2020-09-06 22:15:21 +0500

    Updates

---
+++ precache-manifest.5b69e3a03450519011d059e0d8e3b9f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca54776bdb0fd8f843b7a265cfcc5ff",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "7c3eba015475e1f4596c",
+    "url": "/drum-machine/static/css/main.096edaa6.chunk.css"
+  },
+  {
+    "revision": "3744737a37b645292876",
+    "url": "/drum-machine/static/js/2.41203125.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/drum-machine/static/js/2.41203125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3eba015475e1f4596c",
+    "url": "/drum-machine/static/js/main.9d11c4c8.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
